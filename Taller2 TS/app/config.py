6328ef6652595f06bb5008af4bda0a78e2a1d287
Alejandro Zapata <133360368+ALEZAGI3408@users.noreply.github.com>
2025-05-06 01:01:00 -0500

    Delete Taller2 TS directory

--- Taller2 TS/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-por-defecto'
-    S3_BUCKET = os.environ.get('pokebucket2.0')
-    S3_KEY = os.environ.get('ASIAUOFEB7K7M7353TPZ')
-    S3_SECRET = os.environ.get('GAYC0kIA0fa/0gyDAJAxy+cbCg5jkiZISKuunJmg')
-    S3_LOCATION = f'https://{S3_BUCKET}.s3.amazonaws.com/'
